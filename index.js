ca582f56ff6a0f4da6fabcfdd9281b0430c79ba4
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2022-10-24 22:16:41 -0500

    js file saved and updated

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ console.log(without)
 let shuffle = _.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 console.log(shuffle)
 
+////////////////////////////////////
+
 function gcd_two_numbers(x, y) {
     if ((typeof x !== 'number') || (typeof y !== 'number')) 
       return false;
